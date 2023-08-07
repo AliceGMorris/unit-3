aa2aefd5962a8beba7ddd39ffd3094af1768823a
AliceGMorris <agmorris@wisc.edu>
2023-08-07 12:10:44 -0600

    Activity 10

--- js/main.js
+++ js/main.js
@@ -1,71 +1,149 @@
-//begin script when window loads
-window.onload = setMap();
+(function(){
+	
+	//pseudo-global variables
+	var attrArray = ["Pop", "Pop/Km", "PercWelsh", "HigherEd", "NEETSPerc"]; //list of attributes
+	var expressed = attrArray[2]; //initial attribute
+	
+	//begin script when window loads
+	window.onload = setMap();
 
-//set up choropleth map
-function setMap(){
-	//map frame dimensions
-	var width = 940,
-		height = 500;
-	
-	//create new svg container for the map
-	var map = d3.select("body")
-	.append("svg")
-	.attr("class", "map")
-	.attr("width", width)
-	.attr("height", height);
-	
-	//create Albers equal area conic projection centered on wales
-	var projection = d3.geoAlbers()
-	.center([-5.5, 52.4])
-	.rotate([-2, 0])
-	.parallels([51, 53])
-	.scale(12000)
-	.translate([width / 2, height / 2]);
-	
-	var path = d3.geoPath()
-		.projection(projection);
-	
-	//use Promise.all to parallelize asynchronous data loading
-	var promises = []; 
-	promises.push(d3.csv("data/WalesData.csv")); //load attributes from csv 
-	promises.push(d3.json("data/EuropeCountries2.topojson")); //load background spatial data 
-	promises.push(d3.json("data/WalesRegions.topojson")); //load choropleth spatial data 
-	Promise.all(promises).then(callback);
-	
-	function callback(data) {
-		var csvData = data[0],
-			europe = data[1],
-			wales = data[2];
+	//set up choropleth map
+	function setMap(){
+		//map frame dimensions
+		var width = window.innerWidth * 0.5,
+			height = 460;
+		
+		//create new svg container for the map
+		var map = d3.select("body")
+		.append("svg")
+		.attr("class", "map")
+		.attr("width", width)
+		.attr("height", height);
+		
+		//create Albers equal area conic projection centered on wales
+		var projection = d3.geoAlbers()
+		.center([-5.5, 52.4])
+		.rotate([-2, 0])
+		.parallels([51, 53])
+		.scale(12000)
+		.translate([width / 2, height / 2]);
+		
+		var path = d3.geoPath()
+			.projection(projection);
+		
+		//use Promise.all to parallelize asynchronous data loading
+		var promises = []; 
+		promises.push(d3.csv("data/WalesData.csv")); //load attributes from csv 
+		promises.push(d3.json("data/EuropeCountries2.topojson")); //load background spatial data 
+		promises.push(d3.json("data/WalesRegions.topojson")); //load choropleth spatial data 
+		Promise.all(promises).then(callback);
+		
+		function callback(data) {
+			var csvData = data[0],
+				europe = data[1],
+				wales = data[2];
+				
+			//place graticule on the map
+			setGraticule(map, path);
 
-		//translate europe TopoJSON
-		var europeCountries = topojson.feature(europe, europe.objects.EuropeCountries),
-			walesRegions = topojson.feature(wales, wales.objects.Wales).features;
-		
-		//create graticule generator
-		var graticule = d3.geoGraticule()
-		.step([2, 2]); //place graticule lines every 5 degrees of longitude and latitude
-		
-		//create graticule background
-		var gratBackground = map.append("path")
-			.datum(graticule.outline()) //bind graticule background
-			.attr("class", "gratBackground") //assign class for styling
-			.attr("d", path) //project graticule
-		
-		//create graticule lines
-		var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
-		.data(graticule.lines()) //bind graticule lines to each element to be created
-		.enter() //create an element for each datum
-		.append("path") //append each element to the svg as a path element
-		.attr("class", "gratLines") //assign class for styling
-		.attr("d", path); //project graticule lines
-		
-		//add Europe countries to map
-		var countries = map.append("path")
-			.datum(europeCountries)
-			.attr("class", "countries")
-			.attr("d", path);
+			//translate europe TopoJSON
+			var europeCountries = topojson.feature(europe, europe.objects.EuropeCountries),
+				walesRegions = topojson.feature(wales, wales.objects.Wales).features;
+				
+			//add Europe countries to map
+			var countries = map.append("path")
+				.datum(europeCountries)
+				.attr("class", "countries")
+				.attr("d", path);
+				
+			//join csv data to GeoJSON enumeration units
+			walesRegions = joinData(walesRegions, csvData);
+			
+			//create the color scale
+			var colorScale = makeColorScale(csvData);
+			
+			//add enumeration units to the map
+			setEnumerationUnits(walesRegions, map, path, colorScale);
+			
+			//add coordinated visualization to the map
+			setChart(csvData, colorScale);
+		};
+	};
+	
+	//function to create color scale generator
+	function makeColorScale(data){
+		var colorClasses = [
+			"#D4B9DA",
+			"#C994C7",
+			"#DF65B0",
+			"#DD1C77",
+			"#980043"
+		];
 
-		//add wales regions to map
+		//create color scale generator
+		var colorScale = d3.scaleQuantile()
+			.range(colorClasses);
+
+		var domainArray = [];
+		for (var i=0; i<data.length; i++){
+			var val = parseFloat(data[i][expressed]);
+			domainArray.push(val);
+		};
+
+		//assign array of expressed values as scale domain
+		colorScale.domain(domainArray);
+
+		return colorScale;
+	};
+	
+	function joinData(walesRegions, csvData){	
+		//loop through csv to assign each set of csv attribute values to geojson region
+		for (var i=0; i<csvData.length; i++){
+			var csvRegion = csvData[i]; //the current region
+			var csvKey = csvRegion.name; //the CSV primary key
+
+			//loop through geojson regions to find correct region
+			for (var a=0; a<walesRegions.length; a++){
+
+				var geojsonProps = walesRegions[a].properties; //the current region geojson properties
+				var geojsonKey = geojsonProps.name; //the geojson primary key
+
+				//where primary keys match, transfer csv data to geojson properties object
+				if (geojsonKey == csvKey){
+					
+					//assign all attributes and values
+					attrArray.forEach(function(attr){
+						var val = parseFloat(csvRegion[attr]); //get csv attribute value
+						geojsonProps[attr] = val; //assign attribute and value to geojson properties
+					});
+				};
+			};
+		};
+		return walesRegions;
+	};
+	
+	function setGraticule(map, path){
+			//create graticule generator
+			var graticule = d3.geoGraticule()
+			.step([2, 2]); //place graticule lines every 5 degrees of longitude and latitude
+			
+			//create graticule background
+			var gratBackground = map.append("path")
+				.datum(graticule.outline()) //bind graticule background
+				.attr("class", "gratBackground") //assign class for styling
+				.attr("d", path) //project graticule
+			
+			//create graticule lines
+			var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
+			.data(graticule.lines()) //bind graticule lines to each element to be created
+			.enter() //create an element for each datum
+			.append("path") //append each element to the svg as a path element
+			.attr("class", "gratLines") //assign class for styling
+			.attr("d", path); //project graticule lines
+	};
+	
+	function setEnumerationUnits(walesRegions, map, path, colorScale){
+		//add Welsh regions to map
 		var regions = map.selectAll(".regions")
 			.data(walesRegions)
 			.enter()
@@ -73,6 +151,85 @@ function setMap(){
 			.attr("class", function(d){
 				return "regions " + d.properties.name;
 			})
-			.attr("d", path);
+			.attr("d", path)
+			.style("fill", function(d){            
+                var value = d.properties[expressed];            
+                if(value) {                
+                    return colorScale(d.properties[expressed]);            
+                } else {                
+                    return "#ccc";            
+                }    
+			});
 	};
-};
+	
+	//function to create coordinated bar chart
+	function setChart(csvData, colorScale){
+		//chart frame dimensions
+		var chartWidth = window.innerWidth * 0.425,
+			chartHeight = 460;
+
+		//create a second svg element to hold the bar chart
+		var chart = d3.select("body")
+			.append("svg")
+			.attr("width", chartWidth)
+			.attr("height", chartHeight)
+			.attr("class", "chart");
+		
+		//create a scale to size bars proportionally to frame
+		var yScale = d3.scaleLinear()
+			.range([0, chartHeight])
+			.domain([0, 105]);
+		
+		//set bars for each province
+		var bars = chart.selectAll(".bars")
+			.data(csvData)
+			.enter()
+			.append("rect")
+			.sort(function(a, b){
+				return a[expressed]-b[expressed]
+			})
+			.attr("class", function(d){
+				return "bars " + d.name;
+			})
+			.attr("width", chartWidth / csvData.length - 1)
+			.attr("x", function(d, i){
+				return i * (chartWidth / csvData.length);
+			})
+			.attr("height", function(d){
+				return yScale(parseFloat(d[expressed]));
+			})
+			.attr("y", function(d){
+				return chartHeight - yScale(parseFloat(d[expressed]));
+			})
+			.style("fill", function(d){
+				return colorScale(d[expressed]);
+			});
+		//annotate bars with attribute value text
+		var numbers = chart.selectAll(".numbers")
+			.data(csvData)
+			.enter()
+			.append("text")
+			.sort(function(a, b){
+				return a[expressed]-b[expressed]
+			})
+			.attr("class", function(d){
+				return "numbers " + d.name;
+			})
+			.attr("text-anchor", "middle")
+			.attr("x", function(d, i){
+				var fraction = chartWidth / csvData.length;
+				return i * fraction + (fraction - 1) / 2;
+			})
+			.attr("y", function(d){
+				return chartHeight - yScale(parseFloat(d[expressed])) + 15;
+			})
+			.text(function(d){
+				return Math.round(d[expressed]) + "%";
+			});
+		var chartTitle = chart.append("text")
+			.attr("x", 20)
+			.attr("y", 40)
+			.attr("class", "chartTitle")
+			.text("Percentage of Welsh speakers in each region");
+	};
+})();
